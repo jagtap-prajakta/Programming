7a853d269772cf1b91d61bf24c649f461faafa34
jagtap-prajakta <prajaktajagtap5414@gmail.com>
2026-09-10 15:05:11 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,14 @@
+# 10 Sep 2026:
+
+
+# # 1. check if the number is odd or not and print the result as  Number is odd : False without if 
+# number = int(input("Enter a number: "))
+# is_odd = number % 2 != 0
+# print("Number is odd : ", is_odd)
+
+# 2. WAP to print age in days. OUTPUT: 3 years = 1095 days
+age_in_years = int(input("Enter age in years : "))
+age_in_days = age_in_years * 365
+print(f'Age in years {age_in_years} = {age_in_days} days')
+
+# 3. 
