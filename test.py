aefe5292322552d3a6d19efdbb89842f8c6f2bcd
jagtap-prajakta <prajaktajagtap5414@gmail.com>
2026-09-10 18:55:32 +0530

    5th and 6th task

--- test.py
+++ test.py
@@ -17,7 +17,24 @@
 # remaining_minutes = minutes % 60
 # print(f'{minutes} is {int(hours)} hours and {remaining_minutes} minutes')
 
-# 4. WAP to extract the last digit of a number. OUTPUT : 1234 : last digit is 4
-number = int(input("Enter a number: "))
-last_digit = number % 10
-print(f'{number} : last digit is {last_digit}')
+# # 4. WAP to extract the last digit of a number. OUTPUT : 1234 : last digit is 4
+# number = int(input("Enter a number: "))
+# last_digit = number % 10
+# print(f'{number} : last digit is {last_digit}')
+
+# # 5. WAP to check if a person is eligible for discount the criteria must be a student and age must be below 21. without if else. input values to take are role and age. OUTPUT: Eligible : True/ False
+# role = input("Enter role (must be a student): ")
+# age = int(input("Enter age: "))
+# eligible = (role == "student") and (age < 21)
+# print("Eligible : ", eligible)
+
+
+#  6. WAP to swap two variables without a third variable, using arithmetic operations, Example: before swap a = 10, b = 20  after swap a = 20, b = 10
+
+a = int(input("Enter value for a: "))
+b = int(input("Enter value for b: "))
+print(f'Before swap a = {a}, b = {b}')
+a = a + b
+b = a - b
+a = a - b
+print(f'After swap a = {a}, b = {b}')
