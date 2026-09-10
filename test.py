84521f1142ecb652dfdd4fe6bb7a1598986c4957
jagtap-prajakta <prajaktajagtap5414@gmail.com>
2026-09-10 15:12:28 +0530

    minutes to hours program

--- test.py
+++ test.py
@@ -6,9 +6,13 @@
 # is_odd = number % 2 != 0
 # print("Number is odd : ", is_odd)
 
-# 2. WAP to print age in days. OUTPUT: 3 years = 1095 days
-age_in_years = int(input("Enter age in years : "))
-age_in_days = age_in_years * 365
-print(f'Age in years {age_in_years} = {age_in_days} days')
+# # 2. WAP to print age in days. OUTPUT: 3 years = 1095 days
+# age_in_years = int(input("Enter age in years : "))
+# age_in_days = age_in_years * 365
+# print(f'Age in years {age_in_years} = {age_in_days} days')
 
-# 3. 
+# 3. WAP to covert minutes into hours and print it. OUTPUT: 135 is 2 hours and 15 minutes
+minutes = int(input("Enter minutes: "))
+hours = minutes / 60
+remaining_minutes = minutes % 60
+print(f'{minutes} is {int(hours)} hours and {remaining_minutes} minutes')
